377dc9f602b2ed947ff241de5845442e918b1d57
k-pulkit <pulkit42041@gmail.com>
2022-11-01 10:46:59 -0400

    Fixed glue bookmarks, which was not working, + grouping of files now working. Handled exception when no new data is available and Glue throws Analysis exception

--- glue/job-1.py
+++ glue/job-1.py
@@ -1,5 +1,16 @@
 # type: ignore
 
+"""
+Name - Job-1
+GlueJobName - pknn-aws-twitter-Job-1-toParquet
+Description - This python script makes use of pyspark and glue api
+to read the data from input files located in data catalogs in AWS Glue Tables and
+and loads the data in incremental fashion in parquet format.py
+
+This script requires that the Glue catalog tables are refreshed before running the script, as 
+only the partitions present in the catalog are considered for processing.
+"""
+
 import sys
 from awsglue.transforms import *                
 from awsglue.dynamicframe import DynamicFrame   
@@ -15,7 +26,10 @@ class InitializeGlue(object):
         self.glueContext = GlueContext(self.sc)
         self.spark = self.glueContext.spark_session
         self.job = Job(self.glueContext)
-        self.job.init(self.args['JOB_NAME'])
+        self.job.init(self.args['JOB_NAME'], self.args) # To start record keeping the bookmarks
+        ## Glue partitions are using the partition information for bookmark keys, so
+        #       there can be error when the Glue table refresh and job run is less than an hour
+        #           not checked the same, as currently it is more than 1 hour
         
 class GlueETL_toParquet(InitializeGlue):
     """
@@ -29,31 +43,36 @@ class GlueETL_toParquet(InitializeGlue):
         self.format = "parquet"
         self.groupSize = 1048576  
         
-    def __step1_read_files(self, database_name, table_name, push_down_predicate = ""):
+    def __step1_read_files(self, database_name, table_name, push_down_predicate = "", transformation_ctx=""):
         datasource0 = self.glueContext.create_dynamic_frame_from_catalog(database=database_name,
                                                                  table_name=table_name,
-                                                                 transformation_ctx="datasource0",
-                                                                 additional_options={"groupFiles": "InPartition", "groupSize": self.groupSize},
+                                                                 transformation_ctx=transformation_ctx,
+                                                                 additional_options={"groupFiles": "inPartition", "groupSize": self.groupSize},
                                                                  push_down_predicate = push_down_predicate)
         
-        datasource0.toDF().show(4)
-        
-        return datasource0
+        # Check if the table is empty, that is no new records are available
+        if datasource0.toDF().rdd.isEmpty():
+            return None
+        else:
+            datasource0.toDF().show(4)
+            return datasource0
 
-    def __step2_write_files(self, datasource: DynamicFrame, s3_output_path, partitions=[]):
-        datasink0 = self.glueContext.write_dynamic_frame.from_options(frame=datasource,
-                                                                      connection_type="s3",
-                                                                      connection_options={"path": s3_output_path,
-                                                                                          'partitionKeys': partitions},
-                                                                      transformation_ctx="datasink0",
-                                                                      format = self.format,
-                                                                      format_options={"compression": self.compression})
+    def __step2_write_files(self, datasource: DynamicFrame, s3_output_path, partitions=[], transformation_ctx=""):
+        if datasource:
+            datasink0 = self.glueContext.write_dynamic_frame.from_options(frame=datasource,
+                                                                        connection_type="s3",
+                                                                        connection_options={"path": s3_output_path,
+                                                                                            'partitionKeys': partitions},
+                                                                        transformation_ctx=transformation_ctx,
+                                                                        format = self.format,
+                                                                        format_options={"compression": self.compression})
         
-    def run(self, database_name, table_name, s3_output_path, partitions=[], push_down_predicate = ""):
-        df = self.__step1_read_files(database_name, table_name, push_down_predicate)
-        self.__step2_write_files(df, s3_output_path, partitions)
+    def run(self, database_name, table_name, s3_output_path, partitions=[], push_down_predicate = "", transformation_ctx_1="", transformation_ctx_2=""):
+        df = self.__step1_read_files(database_name, table_name, push_down_predicate, transformation_ctx_1)
+        self.__step2_write_files(df, s3_output_path, partitions, transformation_ctx_2)
         
     def commit(self):
+        # Commit for glue bookmarks
         self.job.commit()
         
 if __name__ == "__main__":
@@ -61,11 +80,12 @@ if __name__ == "__main__":
     etl.run(database_name="twitter_capstone_project_rawzone", \
                 table_name="raw_tweets_json", \
                     s3_output_path="s3://pknn-aws-twitter-stage-zone/stage1/data/raw_tweets_parquet", \
-                        partitions=['year', 'month', 'day', 'hour'])
+                        partitions=['year', 'month', 'day', 'hour'], transformation_ctx_1="datasource01", transformation_ctx_2="datasink01")
     
     etl.run(database_name="twitter_capstone_project_rawzone", \
                 table_name="sp500", \
-                        s3_output_path="s3://pknn-aws-twitter-stage-zone/stage1/data/sp500_parquet")
+                        s3_output_path="s3://pknn-aws-twitter-stage-zone/stage1/data/sp500_parquet"\
+                            , transformation_ctx_1="datasource02", transformation_ctx_2="datasink02")
     
     etl.commit()
     
